bad6fad2bd5d201d359ba88a8f34676259c12998
Welton King <kawaiisungames@gmail.com>
2020-10-23 14:22:38 -0500

    Simple Dialogue and Unity Update

---
+++ Assets/Scripts/Systems/Dialogue.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class Speech
+{
+    public string name;
+    public List<string> messages;
+    public UnityEvent onFinished;
+    public bool canRepeat;
+}
+
+public class Dialogue : MonoBehaviour
+{
+    private bool isTalking;
+    private int messageCounter;
+    private Speech speech;
+    private TextBubble textBubble;
+    private List<int> history = new List<int>();
+
+    public List<Speech> speeches;
+    public GameObject textBubblePrefab;
+    public Transform textBubbleLocation;
+    public float textSpeed;
+    public float bubbleLife;
+
+    public void Talk (int id)
+    {
+        if (history.Contains(id))
+        {
+            if (!speeches[id].canRepeat) 
+                return;
+        }
+        else
+        {
+            history.Add(id);
+        }
+
+        if (isTalking) StopTalking();
+
+        isTalking = true;
+        messageCounter = 0;
+        speech = speeches[id];
+
+        NextBubble();
+    }
+
+    public void NextBubble ()
+    {
+        if (textBubble)
+        {
+            Destroy(textBubble.gameObject);
+            textBubble = null;
+        }
+
+        if (speech == null) { StopTalking(); return; }
+        if (messageCounter >= speech.messages.Count) { StopTalking(); return; }
+
+        GameObject newBubble = Instantiate(textBubblePrefab, textBubbleLocation) as GameObject;
+        newBubble.transform.localPosition = Vector3.zero;
+        textBubble = newBubble.GetComponent<TextBubble>();
+        textBubble.Play(speech.messages[messageCounter], textSpeed, bubbleLife);
+        textBubble.onComplete.AddListener(NextBubble);
+        messageCounter++;
+    }
+
+    public void StopTalking ()
+    {
+        if (textBubble)
+        {
+            Destroy(textBubble.gameObject);
+            textBubble = null;
+        }
+
+        isTalking = false;
+        messageCounter = 0;
+
+        speech.onFinished.Invoke();
+
+        if (!isTalking) speech = null;
+    }
+}
---
+++ Assets/Scripts/Systems/TextBubble.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class TextBubble : MonoBehaviour
+{
+    public TextMeshPro textMesh;
+    public UnityEvent onComplete = new UnityEvent();
+
+    private int character;
+    private string message;
+    private float life;
+    private Transform cam;
+
+    public void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    public void Update()
+    {
+        transform.LookAt(cam);
+    }
+
+    public void Play (string message, float speed, float life)
+    {
+        textMesh.text = "";
+        textMesh.ForceMeshUpdate();
+        this.message = message;
+        this.life = life;
+        StartCoroutine(Speak(1f / speed));
+    }
+
+    public void Complete ()
+    {
+        onComplete.Invoke();
+    }
+
+    public IEnumerator Speak (float wait)
+    {
+        yield return new WaitForSeconds(wait);
+
+        if (string.IsNullOrEmpty(message))
+        {
+            StartCoroutine(Live(life));
+        }
+
+        else
+        {
+            character++;
+
+            textMesh.text = message.Substring(0, Mathf.Min(character, message.Length));
+            textMesh.ForceMeshUpdate();
+
+            if (character >= message.Length)
+            {
+                StartCoroutine(Live(life));
+            }
+
+            else
+            {
+                StartCoroutine(Speak(wait));
+            }
+        }
+    }
+
+    public IEnumerator Live (float wait)
+    {
+        yield return new WaitForSeconds(wait);
+        Complete();
+    }
+}
